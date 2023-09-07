4f187056f597f52bebde2bba9ab86d94513316f5
Kate <kat@mail.com>
2023-09-07 14:51:06 +0200

    started working on training.py

---
+++ train.py
@@ -0,0 +1,11 @@
+""" 1. Train
+
+Train a new network on a data set with train.py
+
+    Basic usage: python train.py data_directory
+    Prints out training loss, validation loss, and validation accuracy as the network trains
+    Options: 
+    * Set directory to save checkpoints: python train.py data_dir --save_dir save_directory 
+    * Choose architecture: python train.py data_dir --arch "vgg13" 
+    * Set hyperparameters: python train.py data_dir --learning_rate 0.01 --hidden_units 512 --epochs 20 
+    * Use GPU for training: python train.py data_dir --gpu """
