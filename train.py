a0eb6428414cfc596f23f887d13200b0fd6d5706
kattat13 <93825970+kattat13@users.noreply.github.com>
2023-09-21 10:06:46 +0200

    added function to check save_dir if exists

--- train.py
+++ train.py
@@ -92,116 +92,148 @@ def data_loader(data, batch_size=64, shuffle=False):
     return loader
 
 
+def create_model(architecture='vgg16'):
+    exec("model = models.{}(pretrained=True)".format(architecture))
+    print(f'Using {architecture} pretrained network...')
+    
+    # freeze parameters
+#     for param in model.parameters():
+#         param.requires_grad = False
+        
+    return model
+
+
+
+def check_save_dir(save_dir):
+    print(save_dir)
+    if save_dir == None:
+        save_dir = 'save_directory'
+        return save_dir
+    else:  # provided
+        if os.path.isdir(save_dir):  # provided and exists
+            return save_dir
+        else:
+            print("\tSpecified directory for saving the model doesn't exist. Application will end.")
+
+
+
+
 parser = argparse.ArgumentParser(description='Train and save an image classification model.')
 parser.add_argument('data_dir', help='The directory of images you want the model to be trained on', type=str)
-parser.add_argument('--save_dir', help='Directory to save checkpoints', type=str)
+parser.add_argument('--save_dir', help='Directory to save checkpoints', type=str, default='save_directory')
+parser.add_argument('--architecture', help='Architecture of the network', default='densenet161')
 
 args = parser.parse_args()
 
-data_dir = args.data_dir
+data_dir  = args.data_dir
 train_dir = data_dir + '/train'
 valid_dir = data_dir + '/valid'
-test_dir = data_dir + '/test'
+test_dir  = data_dir + '/test'
+
+
+
+
 
 if torch.cuda.is_available():
     print('-'*10, '\nCUDA used:', torch.cuda.memory_allocated(), '\n', '-'*10)
     device = torch.device('cuda')
+else:
+    print("CUDA was not found on device, using CPU instead.")
+    device = torch.device('cpu')
 
 if data_dir:
-    if args.save_dir:
-        if os.path.isdir(args.save_dir):
-            # transform data for training, testing and validation sets
-            # and then load the datasets with ImageFolder
-            train_data = test_transformer(train_dir)
-            test_data = train_transformer(test_dir)
-            val_data = train_transformer(valid_dir)
+    if check_save_dir(args.save_dir):
+        # transform data for training, testing and validation sets
+        # and then load the datasets with ImageFolder
+        train_data = test_transformer(train_dir)
+        test_data = train_transformer(test_dir)
+        val_data = train_transformer(valid_dir)
             
-            # define the dataloaders
-            trainloader = data_loader(train_data, shuffle=True)
-            testloader = data_loader(test_data)
-            valloader = data_loader(val_data)
-            
-            with open('cat_to_name.json', 'r') as f:
-                cat_to_name = json.load(f)
-    
-            model = models.vgg16(pretrained=True)
-            model.to(device)
-    
-            for param in model.parameters():
-                param.requires_grad = False
+        # define the dataloaders
+        trainloader = data_loader(train_data, shuffle=True)
+        testloader = data_loader(test_data)
+        valloader = data_loader(val_data)
+        
+        with open('cat_to_name.json', 'r') as f:
+            cat_to_name = json.load(f)
+        
+        model = create_model(args.architecture)
+#             model = models.vgg16(pretrained=True)
+        model.to(device)
 
-            classifier = nn.Sequential(OrderedDict([
-                ('fc1', nn.Linear(25088, 2048)),
-                ('relu1', nn.ReLU()),
-                ('dropout1', nn.Dropout(0.2)),
-                ('fc2', nn.Linear(2048, 512)),
-                ('relu2', nn.ReLU()),
-                ('dropout2', nn.Dropout(0.2)),
-                ('fc3', nn.Linear(512, 102)),
-                ('output', nn.LogSoftmax(dim=1))
-            ]))
+        for param in model.parameters():
+            param.requires_grad = False
 
-            model.classifier = classifier.to(device)
+        classifier = nn.Sequential(OrderedDict([
+            ('fc1', nn.Linear(25088, 2048)),
+            ('relu1', nn.ReLU()),
+            ('dropout1', nn.Dropout(0.2)),
+            ('fc2', nn.Linear(2048, 512)),
+            ('relu2', nn.ReLU()),
+            ('dropout2', nn.Dropout(0.2)),
+            ('fc3', nn.Linear(512, 102)),
+            ('output', nn.LogSoftmax(dim=1))
+        ]))
 
-            criterion = nn.NLLLoss()
-            optimizer = optim.Adam(model.classifier.parameters(), lr=0.003)
+        model.classifier = classifier.to(device)
 
-            # training
-            epochs = 2
-            for epoch in range(epochs):
-                print('EPOCH', epoch)
+        criterion = nn.NLLLoss()
+        optimizer = optim.Adam(model.classifier.parameters(), lr=0.003)
 
-                # notify the model we are in training mode
-                model.train()
-                train_loss = 0
-                val_loss   = 0
+        # training
+        epochs = 2
+        for epoch in range(epochs):
+            print('EPOCH', epoch)
 
-                print('Training', end='')
-                for n, (images, labels) in enumerate(trainloader):
-                    # move images and label tensors to the default device
+            # notify the model we are in training mode
+            model.train()
+            train_loss = 0
+            val_loss   = 0
+
+            print('Training', end='')
+            for n, (images, labels) in enumerate(trainloader):
+                # move images and label tensors to the default device
+                images, labels = images.to(device), labels.to(device)
+                optimizer.zero_grad()
+                logps = model(images)
+                loss = criterion(logps, labels)
+                loss.backward()
+                optimizer.step()        
+                train_loss += loss.item()
+                if not n % 20:
+                    print('.', end='', flush=True)
+            print(n, 'loops')
+
+            # notify the model we are in eval mode
+            # remember no_grad() for the work
+            model.eval()
+            print('Validating', end='')
+            with torch.no_grad():
+                accuracy = 0
+                for n, (images, labels) in enumerate(valloader):
                     images, labels = images.to(device), labels.to(device)
-                    optimizer.zero_grad()
-                    logps = model(images)
-                    loss = criterion(logps, labels)
-                    loss.backward()
-                    optimizer.step()        
-                    train_loss += loss.item()
-                    if not n % 20:
+                    logps = model.forward(images)
+                    batch_loss = criterion(logps, labels)
+                    val_loss   = batch_loss.item()
+
+                    ps = torch.exp(logps)
+                    top_p, top_class = ps.topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                    if not n % 10:
                         print('.', end='', flush=True)
                 print(n, 'loops')
 
-                # notify the model we are in eval mode
-                # remember no_grad() for the work
-                model.eval()
-                print('Validating', end='')
-                with torch.no_grad():
-                    accuracy = 0
-                    for n, (images, labels) in enumerate(valloader):
-                        images, labels = images.to(device), labels.to(device)
-                        logps = model.forward(images)
-                        batch_loss = criterion(logps, labels)
-                        val_loss   = batch_loss.item()
+            train_loss = train_loss/len(trainloader)
+            val_loss   = val_loss/len(valloader)
 
-                        ps = torch.exp(logps)
-                        top_p, top_class = ps.topk(1, dim=1)
-                        equals = top_class == labels.view(*top_class.shape)
-                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-                        if not n % 10:
-                            print('.', end='', flush=True)
-                    print(n, 'loops')
+            val_accuracy = accuracy/len(valloader)
 
-                train_loss = train_loss/len(trainloader)
-                val_loss   = val_loss/len(valloader)
-
-                val_accuracy = accuracy/len(valloader)
-
-                # print statistics
-                print(f'Training Loss: {train_loss:.3f}')
-                print(f'Validation Loss: {val_loss:.3f}')
-                print(f'Validation Accuracy: {val_accuracy:.3f}')
-            save_model(args.save_dir)
-        else:
-            print("Specified directory for saving the model doesn't exist. Application will end.")
+            # print statistics
+            print(f'Training Loss: {train_loss:.3f}')
+            print(f'Validation Loss: {val_loss:.3f}')
+            print(f'Validation Accuracy: {val_accuracy:.3f}')
+        save_model(args.save_dir)
     
     
     
